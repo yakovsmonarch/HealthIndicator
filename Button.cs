dee9646559f3b1fa9f27d2c7659133e4586b8cdc
yakovsmonarch <medvejonok07@gmail.com>
2020-11-25 10:38:18 +0800

    Refactoring

--- Button.cs
+++ Button.cs
@@ -7,18 +7,19 @@ using UnityEngine.UI;
 public class Button : MonoBehaviour
 {
     [SerializeField] private Slider _sliderHealth;
+    [SerializeField] private float _timeIncrement;
 
     public void OnClickButton(float healthIncrement)
     {
-        StartCoroutine(SmoothChangeHealth(healthIncrement));
+        StartCoroutine(SmoothChangeHealth(healthIncrement, _timeIncrement));
     }
 
-    private IEnumerator SmoothChangeHealth(float healthIncrement)
+    private IEnumerator SmoothChangeHealth(float healthIncrement, float timeIncrement)
     {
         for(int i = 0; i < 100; i++)
         {
             _sliderHealth.value += healthIncrement;
-            yield return new WaitForSeconds(0.025f);
+            yield return new WaitForSeconds(timeIncrement);
         }
     }
 }
