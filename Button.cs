690359c27a4bddbea468c37de012e688153a3a36
yakovsmonarch <medvejonok07@gmail.com>
2020-11-25 10:29:14 +0800

    Renaming the script

--- Button.cs
+++ Button.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AddingButton : MonoBehaviour
+public class Button : MonoBehaviour
 {
     [SerializeField] private Slider _sliderHealth;
 
