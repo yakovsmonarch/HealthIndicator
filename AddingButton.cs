35923607db1a328700e1eb9a21f17150add6cc7d
yakovsmonarch <medvejonok07@gmail.com>
2020-11-25 10:06:48 +0800

    Add script AddingButton

---
+++ AddingButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddingButton : MonoBehaviour
+{
+    [SerializeField] private Slider _sliderHealth;
+
+    public void OnClickButton(float healthIncrement)
+    {
+        StartCoroutine(SmoothChangeHealth(healthIncrement));
+    }
+
+    private IEnumerator SmoothChangeHealth(float healthIncrement)
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            _sliderHealth.value += healthIncrement;
+            yield return new WaitForSeconds(0.025f);
+        }
+    }
+}
